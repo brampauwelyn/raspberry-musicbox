bc5db09f6f0049d8d0ba47c11fa38fb1c25f14c1
brampauwelyn <brampauw@gmail.com>
2019-11-17 15:47:16 +0100

    init mixer

--- music.py
+++ music.py
@@ -2,12 +2,23 @@ from gpiozero import Button
 from signal import pause
 import pygame
 
-bass = pygame.mixer.Sound("/samples/bass.wav")
+pygame.init()
+pygame.mixer.init()
 
+# Adding sounds
+bass = pygame.mixer.Sound("/samples/bass.wav")
+piano = pygame.mixer.Sound("/samples/piano.wav")
+beat = pygame.mixer.Sound("/samples/beat.wav")
+
+# Configure the buttons with the GPIO pins
 btn_red = Button(17)
 btn_blue = Button(27)
 btn_yellow = Button(22)
 
+# Play sounds when buttons are presssed
 btn_red.when_pressed = bass.play
+btn_blue.when_pressed = piano.play
+btn_yellow.when_pressed = beat.play
+
 
 
