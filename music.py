2b0175f912e379fdd657fd4748c92fa1618b6b51
brampauwelyn <brampauw@gmail.com>
2019-11-17 17:16:00 +0100

    :lightbulb: adding code for flashing LEDs

--- music.py
+++ music.py
@@ -1,4 +1,4 @@
-from gpiozero import Button
+from gpiozero import Button, LED
 import os
 import pygame
 from time import sleep
@@ -18,10 +18,19 @@ btn_red = Button(17)
 btn_blue = Button(27)
 btn_yellow = Button(22)
 
+# Configure the LEDS
+led_blue = LED(18)
+
+
+def blue_pressed():
+  piano.play()
+  led_blue.on()
+
 # Play sounds when buttons are presssed
 while True:
   btn_red.when_pressed = bass.play
-  btn_blue.when_pressed = piano.play
+  btn_blue.when_pressed = blue_pressed
+  btn_blue.when_released = led_blue.off
   btn_yellow.when_pressed = beat.play
 
 
---
+++ music2.py
@@ -0,0 +1,12 @@
+from psonic import *
+import time
+
+while True:
+  play(72)
+  sleep(1)
+  play(75)
+  sleep(1)
+  play(79)
+  sleep(1)
+  play(72)
+  sleep(1)
