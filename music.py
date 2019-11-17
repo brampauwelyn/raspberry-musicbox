5035ede87133f43863e38deaaaef26ff1ee65245
brampauwelyn <brampauw@gmail.com>
2019-11-17 16:11:38 +0100

    fix code

--- music.py
+++ music.py
@@ -1,14 +1,20 @@
-from gpiozero import Button
+# from gpiozero import 
+import os
+import pygame
+from time import sleep
+
+pygame.init()
+
+path = os.path.realpath(__file__)
+directory = os.path.dirname(path) 
 
 # Adding sounds
-bass = pygame.mixer.Sound("/home/pi/projects/raspberry-musicbox/samples/bass.wav")
-piano = pygame.mixer.Sound("/home/pi/projects/raspberry-musicbox/samples/piano.wav")
-beat = pygame.mixer.Sound("/home/pi/projects/raspberry-musicbox/samples/beat.wav")
+piano = pygame.mixer.Sound(directory + "/samples/piano.wav")
+bass = pygame.mixer.Sound(directory + "/samples/bass.wav")
+beat = pygame.mixer.Sound(directory + "/samples/beat.wav")
 
-pygame.mixer.init()
-
-def say_hello():
-  print("hello")
+piano.play()
+sleep(1)
 
 # Configure the buttons with the GPIO pins
 btn_red = Button(17)
@@ -16,7 +22,7 @@ btn_blue = Button(27)
 btn_yellow = Button(22)
 
 # Play sounds when buttons are presssed
-btn_red.when_pressed = say_hello
+btn_red.when_pressed = bass.play
 btn_blue.when_pressed = piano.play
 btn_yellow.when_pressed = beat.play
 
