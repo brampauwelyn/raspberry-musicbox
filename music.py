35458f5d34d8657695bff796b56536d2d5a97942
brampauwelyn <brampauw@gmail.com>
2019-11-17 16:23:50 +0100

    :art: cleanup

--- music.py
+++ music.py
@@ -1,4 +1,4 @@
-# from gpiozero import 
+from gpiozero import Button
 import os
 import pygame
 from time import sleep
@@ -22,9 +22,10 @@ btn_blue = Button(27)
 btn_yellow = Button(22)
 
 # Play sounds when buttons are presssed
-btn_red.when_pressed = bass.play
-btn_blue.when_pressed = piano.play
-btn_yellow.when_pressed = beat.play
+while True:
+  btn_red.when_pressed = bass.play
+  btn_blue.when_pressed = piano.play
+  btn_yellow.when_pressed = beat.play
 
 
 
