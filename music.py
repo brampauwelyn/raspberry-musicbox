8c823e9e84b86cefe972b139936c6a671c593b17
brampauwelyn <brampauw@gmail.com>
2019-11-17 16:00:56 +0100

    btn press test

--- music.py
+++ music.py
@@ -10,15 +10,19 @@ beat = pygame.mixer.Sound("/home/pi/projects/raspberry-musicbox/samples/beat.wav
 
 pygame.mixer.init()
 
+def say_hello():
+  print("hello")
+
 # Configure the buttons with the GPIO pins
 btn_red = Button(17)
 btn_blue = Button(27)
 btn_yellow = Button(22)
 
 # Play sounds when buttons are presssed
-btn_red.when_pressed = bass.play
+btn_red.when_pressed = say_hello
 btn_blue.when_pressed = piano.play
 btn_yellow.when_pressed = beat.play
+
 
 
 
